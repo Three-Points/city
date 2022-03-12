f83b5e35a02b5d3bc6aae2b438cdc597c9326a03
roremDev <rorem.94.developer@gmail.com>
2022-03-11 23:35:27 -0600

    feat: ✨ added comment component

---
+++ src/components/Comment/index.js
@@ -0,0 +1,16 @@
+import Card from '../Card'
+import Message from '../../theme/font/Content'
+
+const className = ['flex flex-col'].join(' ')
+
+export default ({ text, ...card }) => {
+    const div = document.createElement('div')
+    div.appendChild(Card({ ...card }))
+
+    const message = Message({ text })
+    message.className = `${message.className} mt-3`
+
+    div.appendChild(message)
+    div.className = className
+    return div
+}
