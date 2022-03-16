7980a3806d9600a9de2f3cde766777861a9e6441
roremDev <rorem.94.developer@gmail.com>
2022-03-15 21:27:02 -0600

    feat: ✨ added layout

---
+++ src/layout/asside.js
@@ -0,0 +1,45 @@
+import ButtonIcon from '@components/Button/ButtonIcon'
+import Comment from '@components/Comment'
+import Chat from '@components/Chat'
+
+const classNameShadow = [
+    'shadow shadow_hide',
+    'z-10 absolute inset-0',
+    'bg-shadow',
+    'hidden md:block',
+    'transition-[opacity_visibility] duration-500 ease-in-out',
+].join(' ')
+const classNameAsside = [
+    'asside asside_hide',
+    'z-20 fixed',
+    'w-full top-0 bottom-0 right-0 p-6',
+    'bg-white',
+    'flex flex-col justify-between items-end',
+    'md:w-[397px]  md:p-[72px_36px]',
+    'transition-[transform_visibility] duration-500 ease-in-out',
+].join(' ')
+
+export default ({ onclick }) => {
+    const asside = `
+        <span class="${classNameShadow}"></span>
+        <div class="${classNameAsside}">
+            <div class="grid justify-end justify-items-end gap-x-[24px] w-full">
+                ${ButtonIcon({
+                    style: 'm-0 sm:mx-0',
+                    icon: 'Cross',
+                    onclick,
+                })}
+                ${Comment({
+                    avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+                    name: 'John Doe',
+                    label: 'Published in Adventure',
+                    text: 'Simplefly awaseome photo of natural environment. World is beautiful!!',
+                })}
+            </div>
+            <div class="grid items-end gap-x-[24px] w-full">
+                ${Chat()}
+            </div>
+        </div>
+        `
+    return asside
+}
